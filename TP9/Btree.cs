174a42362e39fa62ee5e2bdc66b0a8f355d48504
Epita <epita@r03p10.cisco.sm.cri.epita.net>
2016-11-17 12:06:35 +0100

    tpcs9

---
+++ TP9/Btree.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TP9
+{
+	public class Btree
+	{
+		private Node root;
+
+		public Btree(Node node)
+		{
+			root = node;
+		}
+
+		public Node Root
+		{
+			get{ return root; }
+			set{ root = value; }
+		}
+
+		public int size(Node node)
+		{
+			if (node == null) 
+			{
+				return 0;
+			}
+
+			return 1 + size(node.Lchild)+size(node.Rchild)
+		}
+
+		public int depth(Node node)
+		{
+			return node == null ? -1 : 1 + Math.Max (depth (node.Lchild), depth (node.Rchild));
+		}
+
+		public void depthDisplay(Node node,string order)
+		{
+			if (node == null) 
+			{
+				return;
+			} 
+
+			if (order == "prefix") 
+			{
+				Console.Write (node.Key + " ");
+			}
+
+			depthDisplay (node.Lchild, order);
+
+			if (order == "infix") 
+			{
+				Console.Write (node.Key + " ");
+			}
+			depthDisplay (node.Rchild, order);
+
+			if (order == "suffixe") 
+			{
+				Console.Write (node.Key + " ");
+			}
+		}
+
+		public void BreathDisplay(Node node)
+		{
+			if (this == null)
+				return;
+			Queue<Node> q = new Queue<Node> ();
+			q.Enqueue (root);
+			while (q.Count != 0) 
+			{
+				Node cur = q.Dequeue ();
+				Console.Write (cur.Key + " ");
+				if (root.Lchild != null)
+					q.Enqueue (cur.Lchild);
+				if (root.Rchild != null)
+					q.Enqueue (cur.Rchild);
+			}
+
+		}
+
+		public Node addNode(Node node,int data)
+		{
+			
+		}
+
+		public bool searchNode(Node node,int s)
+		{
+			
+		}
+
+		public int maxValue(Node node)
+		{
+			
+		}
+
+		public void deleteNode(Node node,int s)
+		{
+			
+		}
+	}
+}
+
---
+++ TP9/Node.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace TP9
+{
+	public class Node
+	{
+		private int _key;
+		private Node _rchild;
+		private Node _lchild;
+
+		public Node(int key,Node lchild,Node rchild)
+		{
+			_key = key;
+			_rchild = rchild;
+			_lchild = lchild;
+		}
+
+		public int Key
+		{
+			get{return _key; }
+			set{ _key = value; }
+		}
+
+		public Node Lchild
+		{
+			get{ return _lchild; }
+			set{ _lchild = value; }
+		}
+
+		public Node Rchild
+		{
+			get{ return _rchild; }
+			set{ _rchild = value; }
+		}
+
+	
+	}
+}
+
